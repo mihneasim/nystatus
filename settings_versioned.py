9e8a6c65cbe1500551a9efeec8886a92fd337cd5
Mihnea Simian <contact@mesimian.com>
2011-09-23 12:06:08 +0300

    Easier deployment, added South

--- settings_versioned.py
+++ settings_versioned.py
@@ -18,12 +18,14 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = os.path.dirname(os.path.realpath(__file__)) + '/db' # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+ABS_ROOT = os.path.dirname(os.path.realpath(__file__))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ABS_ROOT + '/db'
+    }
+}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -87,5 +89,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.admin',
-    'django_zopeproductversions'
+    'django_zopeproductversions',
+    'south'
 )
